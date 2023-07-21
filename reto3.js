0e02b51674b08f3a27b8ed836f83ff33396e05c3
JhoanDucu <114232996+JhoanDucu@users.noreply.github.com>
2023-07-21 11:01:46 -0500

    Update reto3.js

--- reto3.js
+++ reto3.js
@@ -1,9 +1,9 @@
-let num1 = int(prompt('h'));
-let num2 = prompt();
+let num1 = Number(prompt('Ingrese numero'));
+let num2 = Number(prompt('Ingrese otro numero'));
 
 let suma = num1 + num2;
 
-let num3 = prompt();
+let num3 = Number(prompt('Ingrese un tercer numero: '));
 let multiplicacion = suma * num3;
 
 console.log('El resultado es', multiplicacion);
