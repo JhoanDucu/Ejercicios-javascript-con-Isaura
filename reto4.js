daef45bc5d856efb045a4e2deaaa5f44ff9c464f
JhoanDucu <114232996+JhoanDucu@users.noreply.github.com>
2023-07-21 11:03:31 -0500

    Update reto4.js

--- reto4.js
+++ reto4.js
@@ -1,5 +1,5 @@
-let km = prompt(Int('Ingrese el numero de kilometros que recorrio su motocicleta: '));
-let combustible = prompt(int('Ingrese el total de gasolina gastada: '));
+let km = Number(prompt('Ingrese el numero de kilometros que recorrio su motocicleta: '));
+let combustible = Number(prompt('Ingrese el total de gasolina gastada: '));
 
 let comPerKm = combustible/km;
 console.log('Usted gasto ', comPerKm, 'de combustible por kilometro');
